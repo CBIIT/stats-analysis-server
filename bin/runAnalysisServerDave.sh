a2e980085aba90309d286492357891eb661e17b0
bauerd <bauerd>
2005-12-08 19:13:30 +0000

    *** empty log message ***
    SVN-Revision: 11713

--- bin/runAnalysisServerDave.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-nohup /usr/jdk1.5.0_04/bin/java -classpath ./:../lib/concurrent.jar:../lib/jboss-common.jar:../lib/jbossmq-client.jar:../lib/jnp-client.jar:../lib/jboss-common-client.jar:../lib/jboss-system-client.jar:../lib/jms.jar:../lib/log4j.jar:../lib/Rserve.jar:../lib/runClasses.jar gov.nih.nci.caintegrator.analysis.server.AnalysisServer analysisServerDave.properties > analysisServerDave.log &
