b605ce6240a7797635d6ed82f0e76994783c2ccc
Harris Michael <harrismic>
2006-08-06 20:33:50 +0000

    CompoundRequest for handling  group computations.
    SVN-Revision: 11887

---
+++ src/gov/nih/nci/caintegrator/analysis/server/CompoundRequestTaskR.java
@@ -0,0 +1,87 @@
+package gov.nih.nci.caintegrator.analysis.server;
+
+import org.apache.log4j.Logger;
+
+import gov.nih.nci.caintegrator.analysis.messaging.AnalysisRequest;
+import gov.nih.nci.caintegrator.analysis.messaging.AnalysisResult;
+import gov.nih.nci.caintegrator.analysis.messaging.ClassComparisonRequest;
+import gov.nih.nci.caintegrator.analysis.messaging.CompoundAnalysisRequest;
+import gov.nih.nci.caintegrator.analysis.messaging.CompoundAnalysisResult;
+import gov.nih.nci.caintegrator.analysis.messaging.HierarchicalClusteringRequest;
+import gov.nih.nci.caintegrator.analysis.messaging.PrincipalComponentAnalysisRequest;
+import gov.nih.nci.caintegrator.exceptions.AnalysisServerException;
+
+public class CompoundRequestTaskR extends AnalysisTaskR {
+
+	
+	private CompoundAnalysisResult result = null;
+	
+	private static Logger logger = Logger.getLogger(CompoundRequestTaskR.class);
+
+	
+	
+	public CompoundRequestTaskR(CompoundAnalysisRequest compoundRequest) {
+		super(compoundRequest);
+		
+	}
+
+	public CompoundRequestTaskR(CompoundAnalysisRequest compoundRequest, boolean debugRcommands) {
+		super(compoundRequest, debugRcommands);
+		
+	}
+
+	@Override
+	public void run() {
+		CompoundAnalysisRequest compoundRequest = (CompoundAnalysisRequest) getRequest();
+		
+		result = new CompoundAnalysisResult(compoundRequest.getSessionId(), compoundRequest.getTaskId());
+		
+		for (AnalysisRequest request : compoundRequest.getRequests()) {
+			//execute the request
+			logger.info("CompoundRequestTaskR: running request: " + request);
+			runRequest(request);
+		}
+
+	}
+
+	private void runRequest(AnalysisRequest request) {
+		
+	  AnalysisTaskR task = null;	
+	
+	  if (request instanceof ClassComparisonRequest) {
+	    task = new ClassComparisonTaskR((ClassComparisonRequest) request);
+	  }
+	  else if (request instanceof PrincipalComponentAnalysisRequest) {
+	    task = new PrincipalComponentAnalysisTaskR((PrincipalComponentAnalysisRequest)request);
+	  }
+	  else if (request instanceof HierarchicalClusteringRequest){
+		task = new HierarchicalClusteringTaskR((HierarchicalClusteringRequest) request);
+	  }
+	
+      try {
+		task.setRComputeConnection(this.getRComputeConnection());
+		task.run();
+		result.addResult(task.getResult());
+		task.cleanUp();
+	  } catch (AnalysisServerException e) {
+		this.setException(e);
+	  }
+	}
+
+	@Override
+	public void cleanUp() {
+		try {
+			setRComputeConnection(null);
+		} catch (AnalysisServerException e) {
+			logger.error("Error in cleanUp method.");
+			logger.error(e);
+			setException(e);
+		}
+	}
+
+	@Override
+	public AnalysisResult getResult() {
+		return result;
+	}
+
+}
