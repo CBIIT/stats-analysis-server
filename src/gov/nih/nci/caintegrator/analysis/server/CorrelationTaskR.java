670ebe423821e5e713e5e84ca8403a312404b74d
Harris Michael <harrismic>
2006-08-06 20:45:56 +0000

    Place holder classes.
    SVN-Revision: 11889

---
+++ src/gov/nih/nci/caintegrator/analysis/server/CorrelationTaskR.java
@@ -0,0 +1,36 @@
+package gov.nih.nci.caintegrator.analysis.server;
+
+import gov.nih.nci.caintegrator.analysis.messaging.AnalysisRequest;
+import gov.nih.nci.caintegrator.analysis.messaging.AnalysisResult;
+
+public class CorrelationTaskR extends AnalysisTaskR {
+
+	public CorrelationTaskR(AnalysisRequest request) {
+		super(request);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CorrelationTaskR(AnalysisRequest request, boolean debugRcommands) {
+		super(request, debugRcommands);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void cleanUp() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public AnalysisResult getResult() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/gov/nih/nci/caintegrator/analysis/server/FTestTaskR.java
@@ -0,0 +1,36 @@
+package gov.nih.nci.caintegrator.analysis.server;
+
+import gov.nih.nci.caintegrator.analysis.messaging.AnalysisRequest;
+import gov.nih.nci.caintegrator.analysis.messaging.AnalysisResult;
+
+public class FTestTaskR extends AnalysisTaskR {
+
+	public FTestTaskR(AnalysisRequest request) {
+		super(request);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FTestTaskR(AnalysisRequest request, boolean debugRcommands) {
+		super(request, debugRcommands);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void cleanUp() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public AnalysisResult getResult() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
