2c134bedeb3cb504e42525d4a5087d7f78cab288
Harris Michael <harrismic>
2006-09-21 18:07:24 +0000

    Added more logging to catch clauses.
    SVN-Revision: 11917

--- src/gov/nih/nci/caintegrator/analysis/server/CorrelationTaskR.java
+++ src/gov/nih/nci/caintegrator/analysis/server/CorrelationTaskR.java
@@ -1,11 +1,21 @@
 package gov.nih.nci.caintegrator.analysis.server;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Vector;
 
 import gov.nih.nci.caintegrator.analysis.messaging.AnalysisRequest;
 import gov.nih.nci.caintegrator.analysis.messaging.AnalysisResult;
 import gov.nih.nci.caintegrator.analysis.messaging.CorrelationRequest;
 import gov.nih.nci.caintegrator.analysis.messaging.CorrelationResult;
+import gov.nih.nci.caintegrator.analysis.messaging.DataPoint;
 import gov.nih.nci.caintegrator.analysis.messaging.ReporterInfo;
 import gov.nih.nci.caintegrator.enumeration.CorrelationType;
 import gov.nih.nci.caintegrator.exceptions.AnalysisServerException;
@@ -36,30 +46,9 @@ public class CorrelationTaskR extends AnalysisTaskR {
 		result = new CorrelationResult(getRequest().getSessionId(), getRequest().getTaskId());
 		logger.info(getExecutingThreadName() + " processing correlation request="
 						+ corrRequest);
-		REXP rVal;
-		
-		try {
-			
-			String dataFileName = corrRequest.getDataFileName();
-			
-			if (dataFileName == null) {
-			  //check to make sure that the vectors have data
-			  if ((corrRequest.getVector1()==null)||(corrRequest.getVector2()==null)) {
-				throw new AnalysisServerException("Problem with correlation request. No data file specified and vectors are null.");
-			  }	  
-			}
-			else {
-			  setDataFile(corrRequest.getDataFileName());
-			}
-			
-		} catch (AnalysisServerException e) {
-			e.setFailedRequest(corrRequest);
-			logger.error("Internal Error. " + e.getMessage());
-			setException(e);
-			return;
-		}
-
-		
+		double rVal = -1.0;
+		List<DataPoint> points = Collections.emptyList();
+	
 		try {
 		
 			//Check to see if a reporter was passed in
@@ -83,68 +72,103 @@ public class CorrelationTaskR extends AnalysisTaskR {
 			  doRvoidEval(sampleIdscmd);
 			  cmd = "SM <- getSubmatrix.onegrp(dataMatrix, sampleIds)";
 			  doRvoidEval(cmd);			  
-			  cmd = "RM1 <- getSubmatrix.rep(SM," + reporter1.getReporterName() + ")";
-			  doRvoidEval(cmd);
-			 // result.setVector1(RM1);
+			  cmd = "RM1 <- getSubmatrix.rep(SM,\"" + reporter1.getReporterName() + "\")";
+			  double[] vec1 =  doREval(cmd).asDoubleArray();
+			  //need to make sure that the vectors are in the same order wrt sample ids
+			  cmd = "RM1_labels <- dimnames(RM1)[[1]]";
+			  Vector RM1_ids = doREval(cmd).asVector();
 			  
-			  //need to set vector 1 with RM1
-			  
+			  Map <String, DataPoint> vecMap = new HashMap<String, DataPoint>();
+			  String id;
+			  DataPoint point;
+			  for (int i=0; i < RM1_ids.size(); i++) {
+				  //create objects and store.
+				  id = ((REXP)RM1_ids.get(i)).toString();
+				  point = vecMap.get(id);
+				  if (point == null) {
+				    point = new DataPoint(id);
+				    vecMap.put(id, point);
+				  }
+				  point.setX(vec1[i]);
+			  }
 			  
 			  //get the data matrix for reporter 2
 			  setDataFile(reporter2.getDataFileName());
 			  doRvoidEval(sampleIdscmd);
 			  cmd = "SM <- getSubmatrix.onegrp(dataMatrix, sampleIds)";
 			  doRvoidEval(cmd);			  
-			  cmd = "RM2 <- getSubmatrix.rep(SM," + reporter2.getReporterName() + ")";
-				
-			  //need to set vector 2 with RM2
-			  //result.setVector2(RM2);
+			  cmd = "RM2 <- getSubmatrix.rep(SM,\"" + reporter2.getReporterName() + "\")";
+			  double[] vec2 = doREval(cmd).asDoubleArray();
+			  Vector RM2_ids = doREval(cmd).asVector();
+			 
+			  //set the vector with the values for vector2
+			  for (int i=0; i < RM2_ids.size(); i++) {
+				  //create objects and store.
+				  id = ((REXP)RM2_ids.get(i)).toString();
+				  point = vecMap.get(id);
+				  
+				  if (point != null) {
+					point.setY(vec2[i]);				   
+				  }
+				  else {
+					logger.warn("Correlation skipping id=" + id + " not found in data file 2");
+					vecMap.remove(id);
+				  }
+			  }
 			  
+			  points = new ArrayList<DataPoint>(vecMap.values());
+			  
+			  
+			  String v1str = "v1 <- c(" + getDataString(points, true,false);
+			  String v2str = "v2 <- c(" + getDataString(points,false,true);
+			  
+			  doRvoidEval(v1str);
+			  doRvoidEval(v2str);
 			  if (corrRequest.getCorrelationType() == CorrelationType.PEARSON) {
-				  cmd = "r <- correlation(RM1,RM2,\"pearson\")";
-				  rVal = doREval(cmd);
+				  cmd = "r <- correlation(v1,v2,\"pearson\")";
+				  r = doREval(cmd).asDouble();
 				}
 				else if (corrRequest.getCorrelationType() == CorrelationType.SPEARMAN) {
-				  cmd = "r <- correlation(RM1,RM2, \"spearman\")";
-				  rVal = doREval(cmd);
+				  cmd = "r <- correlation(v1,v2, \"spearman\")";
+				  r = doREval(cmd).asDouble();
 				}
 			  
 			}
-			
-			
-			
 			
 			
 			//String cmd = CorrelationTaskR.getRgroupCmd("GRP1", corrRequest.getVector1().getValues());
-			doRvoidEval(cmd);
-			
-			cmd = CorrelationTaskR.getRgroupCmd("GRP2", corrRequest.getVector2().getValues());
-			doRvoidEval(cmd);
+//			doRvoidEval(cmd);
+//			
+//			cmd = CorrelationTaskR.getRgroupCmd("GRP2", corrRequest.getVector2().getValues());
+//			doRvoidEval(cmd);
 			
 			//REXP rVal = null;
 			
-			if (corrRequest.getCorrelationType() == CorrelationType.PEARSON) {
-			  cmd = "r <- correlation(GRP1,GRP2,\"pearson\")";
-			  rVal = doREval(cmd);
-			}
-			else if (corrRequest.getCorrelationType() == CorrelationType.SPEARMAN) {
-			  cmd = "r <- correlation(GRP1,GRP2, \"spearman\")";
-			  rVal = doREval(cmd);
-			}
-			else {
-			  throw new AnalysisServerException("Unrecognized correlationType or correlation type is null.");
-			}
+//			if (corrRequest.getCorrelationType() == CorrelationType.PEARSON) {
+//			  cmd = "r <- correlation(GRP1,GRP2,\"pearson\")";
+//			  rVal = doREval(cmd);
+//			}
+//			else if (corrRequest.getCorrelationType() == CorrelationType.SPEARMAN) {
+//			  cmd = "r <- correlation(GRP1,GRP2, \"spearman\")";
+//			  rVal = doREval(cmd);
+//			}
+//			else {
+//			  throw new AnalysisServerException("Unrecognized correlationType or correlation type is null.");
+//			}
 	
 			//double r = rVal.asDouble();
 			result.setCorrelationValue(r);
-			result.setVector1(corrRequest.getVector1());
-			result.setVector2(corrRequest.getVector2());
+			//result.setVector1(vec1);
+			//result.setVector2(vec2);
+			result.setDataPoints(points);
 		}
 		catch (AnalysisServerException asex) {
 			AnalysisServerException aex = new AnalysisServerException(
 			"Problem computing correlation. Caught AnalysisServerException in CorrelationTaskR." + asex.getMessage());
 	        aex.setFailedRequest(corrRequest);
 	        setException(aex);
+	        logger.error("Caught AnalysisServerException");
+	        logger.error(asex);
 	        return;  
 		}
 		catch (Exception ex) {
@@ -152,6 +176,11 @@ public class CorrelationTaskR extends AnalysisTaskR {
 			"Internal Error. Caught Exception in CorrelationTaskR exClass=" + ex.getClass() + " msg=" + ex.getMessage());
 	        asex.setFailedRequest(corrRequest);
 	        setException(asex);
+	        logger.error("Caught Exception in CorrelationTaskR");
+	        StringWriter sw = new StringWriter();
+	        PrintWriter pw  = new PrintWriter(sw);
+	        ex.printStackTrace(pw);
+	        logger.error(sw.toString());
 	        return;  
 		}
 	}
@@ -171,5 +200,37 @@ public class CorrelationTaskR extends AnalysisTaskR {
 	public AnalysisResult getResult() {
 		return result;
 	}
-
+	
+	/**
+	 * 
+	 * @param points
+	 * @param useX
+	 * @param useY
+	 * @return Quoted string for use in R command
+	 */
+	private String getDataString(List<DataPoint> points, boolean useX, boolean useY) {
+	  StringBuffer sb = new StringBuffer();
+	  DataPoint point;
+	  for (Iterator i=points.iterator(); i.hasNext(); ) {
+		 point = (DataPoint) i.next();
+		 if (useX) {
+		   sb.append("\"").append(point.getX()).append("\"");
+		 } 
+		 
+		 
+		 if (useY) {
+		   sb.append("\"").append(point.getY()).append("\"");
+		 }
+		 
+		 if (i.hasNext()) {
+		   sb.append(",");
+		 }
+		 else {
+		   sb.append(")");	 
+		 }		
+	  }
+	  return sb.toString();
+      
+	}
+	  
 }
