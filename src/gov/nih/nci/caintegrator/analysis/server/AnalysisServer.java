67d94da8e4657df49cad41a55672501743be99e3
Harris Michael <harrismic>
2006-11-30 21:52:21 +0000

    Changed to version 10.7 for getSubmatrix fixes.
    SVN-Revision: 11967

--- src/gov/nih/nci/caintegrator/analysis/server/AnalysisServer.java
+++ src/gov/nih/nci/caintegrator/analysis/server/AnalysisServer.java
@@ -116,7 +116,7 @@ public class AnalysisServer implements MessageListener, ExceptionListener, Analy
 	/**
 	 * The server version number.
 	 */
-	public static String version = "10.6";
+	public static String version = "10.7";
 
 	private boolean debugRcommands = false;
 
