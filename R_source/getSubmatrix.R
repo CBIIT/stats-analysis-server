027ad5d185f079a54aaaabe5bdb163ae8e20769a
Harris Michael <harrismic>
2006-11-30 21:51:10 +0000

    Fixed getSubmatrix.twogrps because it didn't work when datamat only had one row.
    SVN-Revision: 11966

--- R_source/getSubmatrix.R
+++ R_source/getSubmatrix.R
@@ -8,8 +8,14 @@
 # Generate submatrix based on sample IDs
 getSubmatrix.twogrps <- function(datmat, grp1ids, grp2ids) {
 	allids <- dimnames(datmat)[[2]]
-    Submatrix <- cbind(datmat[,allids%in%grp1ids],datmat[,allids%in%grp2ids])
-    return(as.matrix(Submatrix))
+	if (length(dimnames(datmat)[[1]])==1) {
+	Submatrix <- t(as.matrix(c(datmat[,allids%in%grp1ids],datmat[,allids%in%grp2ids])))
+	dimnames(Submatrix)[[1]] <- dimnames(datmat)[[1]]
+	}
+	else {
+    	Submatrix <- cbind(datmat[,allids%in%grp1ids],datmat[,allids%in%grp2ids])
+	}
+    	return(as.matrix(Submatrix))
 }
 
 # Submatrix <- getSubmatrix.twogrps(datmat, grp1ids, grp2ids)  
