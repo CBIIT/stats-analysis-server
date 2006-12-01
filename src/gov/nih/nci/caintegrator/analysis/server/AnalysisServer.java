a564c8ddb3690e3032c1acec9aae93d4e126ded7
Harris Michael <harrismic>
2006-12-01 18:44:44 +0000

    Changed version number to 10.8
    SVN-Revision: 11974

--- src/gov/nih/nci/caintegrator/analysis/server/AnalysisServer.java
+++ src/gov/nih/nci/caintegrator/analysis/server/AnalysisServer.java
@@ -116,7 +116,7 @@ public class AnalysisServer implements MessageListener, ExceptionListener, Analy
 	/**
 	 * The server version number.
 	 */
-	public static String version = "10.7";
+	public static String version = "10.8";
 
 	private boolean debugRcommands = false;
 
