75eb22335901ca2d85cd4a61aee10f464d56a556
Harris Michael <harrismic>
2007-02-20 17:43:23 +0000

    added chmod commands to the deployment script.
    SVN-Revision: 12038

--- src/templates/deploy.sh
+++ src/templates/deploy.sh
@@ -7,3 +7,7 @@ rm -r /local/content/caintegrator/tempFiles
 tar -xvf /local/content/caintegrator/caintegrator-analysis-server.tar
 rm -r  /local/content/caintegrator/bin
 mv /local/content/caintegrator/bin_bak /local/content/caintegrator/bin
+cd /local/content/caintegrator/bin
+chmod u+x *.sh
+dos2unix *.sh
+echo "Deployment complete"
