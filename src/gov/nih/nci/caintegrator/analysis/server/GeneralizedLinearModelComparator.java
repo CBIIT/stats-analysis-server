ff4b6bf6da6ff8f57158b90d2ae85baa33f09282
Dana Zhang <zhangd>
2007-04-19 21:31:22 +0000

    *** empty log message ***
    SVN-Revision: 12049

---
+++ src/gov/nih/nci/caintegrator/analysis/server/GeneralizedLinearModelComparator.java
@@ -0,0 +1,52 @@
+
+package gov.nih.nci.caintegrator.analysis.server;
+
+import java.util.Comparator;
+import gov.nih.nci.caintegrator.analysis.messaging.GeneralizedLinearModelResultEntry;
+
+
+public class GeneralizedLinearModelComparator implements Comparator {
+	
+	public int compare(Object o1, Object o2) {
+		
+		if(!(o1 instanceof GeneralizedLinearModelResultEntry)) {
+			return 0;
+		   }
+		if(!(o2 instanceof GeneralizedLinearModelResultEntry)) {
+		     return 0;
+	       }
+		
+		GeneralizedLinearModelResultEntry glmResultEntry = (GeneralizedLinearModelResultEntry)o1;
+		GeneralizedLinearModelResultEntry glmResultEntry2 = (GeneralizedLinearModelResultEntry)o2;
+		
+		if(glmResultEntry.getPvalue() < glmResultEntry2.getPvalue()) {
+			return -1;
+			}
+		if(glmResultEntry.getPvalue() > glmResultEntry2.getPvalue()) {
+			return 1;
+			}
+		
+		if(glmResultEntry.getAdjustedPvalue() < glmResultEntry2.getAdjustedPvalue()) {
+			return -1;
+			}
+		if(glmResultEntry.getAdjustedPvalue() > glmResultEntry2.getAdjustedPvalue()) {
+			return 1;
+			}
+		
+		
+		double abFC = Math.abs(glmResultEntry.getFoldChange());
+		double abFC2 = Math.abs(glmResultEntry2.getFoldChange());
+		
+		
+		
+		if(abFC < abFC2) {
+			return -1;
+			}
+		if(abFC > abFC2) {		
+			return 1;
+			}
+		
+		 return 0;
+		}
+	
+}
