a1e1a4fa63fccb010d8e6c14020816db253d82ff
Wei Lin <linw@mail.nih.gov>
2007-04-27 19:16:29 +0000

    This is the code for GLM. There are two functions. "eagle.glm.single" is the GLM on single reporter. "eagle.glm.array" run "eagle.glm.single" across all the reporters with and without covariate adjustment. Java can simply call "eagle.glm.array" by inputing subject id, group id and covariate values. It returns a data frame of the p value before and after adjustment.
    SVN-Revision: 12057

---
+++ R_source/GLM.R
@@ -0,0 +1,128 @@
+##############################################################################################################
+# File: Gneralized Linear Model
+# Description: R module for higher order analysis tools - generalized linear model regression
+# Author: Wei Lin
+# Date: April 2007
+###############################################################################################################
+#
+#  Note: Arguments include the data matrix of expression, subject id,
+#         group id and vector/matrix of the covariate values. 
+#
+#############################################################################################################
+
+eagle.glm.single <- function(rptr_exps, subids, grpids, is.covar=FALSE, covar) ##covs is for data matrix
+{
+	rptrSubExp<-rptr_exps[subids]
+	grps<-as.factor(grpids)
+	uniqGrp<-unique(grpids)
+	uniqGrpCount<-length(uniqGrp)
+	if(is.covar){
+		if(is.null(dim(covar)) & length(covar)==length(grpids)){
+			adjustment<-covar
+			if (mode(as.vector(covar))== "character"){
+				adjustment<-as.factor(covar)
+			}
+			glm<-glm(rptrSubExp~grps+adjustment)
+			summary.glm<-summary(glm)
+			glm.coefs<-summary.glm$coef
+		}
+		else {
+			if (dim(covar)[2]==2){
+				adjustment1<-covar[,1]
+				adjustment2<-covar[,2]
+				if (mode(as.vector(covar[,1]))== "character"){
+					adjustment1<-as.factor(covar[,1])
+				}
+				if (mode(as.vector(covar[,2]))== "character"){
+					adjustment2<-as.factor(covar[,2])
+				}
+				glm<-glm(rptrSubExp~grps+adjustment1+adjustment2)
+				summary.glm<-summary(glm)
+				glm.coefs<-summary.glm$coef
+			}
+			if (dim(covar)[2]==3){
+				adjustment1<-covar[,1]
+				adjustment2<-covar[,2]
+				adjustment3<-covar[,3]
+				if (mode(as.vector(covar[,1]))== "character"){
+					adjustment1<-as.factor(covar[,1])
+				}
+				if (mode(as.vector(covar[,2]))== "character"){
+					adjustment2<-as.factor(covar[,2])
+				}
+				if (mode(as.vector(covar[,3]))== "character"){
+					adjustment3<-as.factor(covar[,3])
+				}
+				glm<-glm(rptrSubExp~grps+adjustment1+adjustment2+adjustment3)
+				summary.glm<-summary(glm)
+				glm.coefs<-summary.glm$coef
+			}
+			if (dim(covar)[2]==4){
+				adjustment1<-covar[,1]
+				adjustment2<-covar[,2]
+				adjustment3<-covar[,3]
+				adjustment4<-covar[,4]
+				if (mode(as.vector(covar[,1]))== "character"){
+					adjustment1<-as.factor(covar[,1])
+				}
+				if (mode(as.vector(covar[,2]))== "character"){
+					adjustment2<-as.factor(covar[,2])
+				}
+				if (mode(as.vector(covar[,3]))== "character"){
+					adjustment3<-as.factor(covar[,3])
+				}
+				if (mode(as.vector(covar[,4]))== "character"){
+					adjustment4<-as.factor(covar[,4])
+				}
+				glm<-glm(rptrSubExp~grps+adjustment1+adjustment2+adjustment3+adjustment4)
+				summary.glm<-summary(glm)
+				glm.coefs<-summary.glm$coef
+			}
+		}
+	}
+	else { 
+		glm<-glm(rptrSubExp~grps);
+		summary.glm<-summary(glm);
+		glm.coefs<-summary.glm$coef;
+	}
+	pValues<-glm.coefs[2:(uniqGrpCount), 4]
+	pValNames<-names(pValues)
+	pValNewNames<-substr(pValNames, 5, 10000)
+	pValNewNames->names(pValues)
+	return(pValues)
+}
+
+eagle.glm.array<- function(datamat, subids, group.ids, is.covar=FALSE, covar){
+	if (is.covar){
+		pv_glm<-apply(datamat, 1, eagle.glm.single, subids, group.ids, is.covar=FALSE)
+		pv_cov<-apply(datamat, 1, eagle.glm.single, subids, group.ids, is.covar=TRUE, covar)
+		
+		pb_grps<-rownames(pv_glm)
+		pb_grps<-paste(pb_grps, "_before_adjustment", sep="")
+		rownames(pv_glm)<-pb_grps
+		
+		pa_grps<-rownames(pv_cov)
+		pa_grps<-paste(pa_grps, "_after_adjustment", sep="")
+		rownames(pv_cov)<-pa_grps
+	
+		Pv_Pairs<-rbind(pv_glm, pv_cov)
+	}
+	else {
+		pv_glm<-apply(datamat, 1, eagle.glm.single, subids, group.ids, is.covar=FALSE)
+		
+		pb_grps<-rownames(pv_glm)
+		pb_grps<-paste(pb_grps, "_before_adjustment", sep="")
+		rownames(pv_glm)<-pb_grps
+		
+		Pv_Pairs<-pv_glm
+	}
+	Pv_Pairs<-t(Pv_Pairs)
+	return(Pv_Pairs)
+}
+
+#ealge.boxplot<- function(rptr_exps, sub_id=subject.ids, grpids=group.ids, reporter=rptr){
+#	rptr_exp<-rptr_exps[rptr, subject.ids]
+#	mainstr<-paste("Boxplot of ", rptr, "Expression Across Groups")
+#	boxplot_rptr<-boxplot(rptr_exp~group.ids, main=mainstr, ylab="Expressions", xlab="Groups")
+#	return(boxplot)
+#}
