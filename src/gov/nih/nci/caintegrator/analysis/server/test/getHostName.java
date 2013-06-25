ee223121c2219aef24ae68f5cdcc9b1bcff7b49c
Prasad Konka <prasad.konka@nih.gov>
2013-06-25 13:43:20 -0400

    Update copyright

--- src/gov/nih/nci/caintegrator/analysis/server/test/getHostName.java
+++ src/gov/nih/nci/caintegrator/analysis/server/test/getHostName.java
@@ -1,3 +1,10 @@
+/*L
+ *  Copyright SAIC
+ *
+ *  Distributed under the OSI-approved BSD 3-Clause License.
+ *  See http://ncip.github.com/stats-analysis-server/LICENSE.txt for details.
+ */
+
 package gov.nih.nci.caintegrator.analysis.server.test;
 
 import java.util.*;
